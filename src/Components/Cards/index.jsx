a0a7177c62702ef815ebd75cda65d11655b7123d
Viktor Kinyua <54580936+amature98@users.noreply.github.com>
2024-04-18 09:06:47 +0300

    More changes and functionalities

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -2,23 +2,43 @@ import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import React from "react";
 import JoinGroupButton from "../Buttons/JoinGroup.button";
 
-function index({ groupName, description, image }) {
+function index({ groupName, description, image, direction }) {
   return (
     <>
       <Card sx={{ display: "flex" }}>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <CardContent sx={{ flex: "1 0 auto" }}>
-            <Typography variant="h5">{groupName}</Typography>
-            <Typography variant="body2"> {description} </Typography>
-            <JoinGroupButton groupName={groupName} />
-          </CardContent>
-        </Box>
-        <CardMedia
-          component="img"
-          sx={{ width: 150 }}
-          image={image}
-          alt="image"
-        />
+        {direction === "left" ? (
+          <>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <Typography variant="h5">{groupName}</Typography>
+                <Typography variant="body2"> {description} </Typography>
+                <JoinGroupButton groupName={groupName} />
+              </CardContent>
+            </Box>
+            <CardMedia
+              component="img"
+              sx={{ width: 150 }}
+              image={image}
+              alt="image"
+            />
+          </>
+        ) : (
+          <>
+            <CardMedia
+              component="img"
+              sx={{ width: 150 }}
+              image={image}
+              alt="image"
+            />
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <Typography variant="h5">{groupName}</Typography>
+                <Typography variant="body2"> {description} </Typography>
+                <JoinGroupButton groupName={groupName} />
+              </CardContent>
+            </Box>
+          </>
+        )}
       </Card>
     </>
   );
