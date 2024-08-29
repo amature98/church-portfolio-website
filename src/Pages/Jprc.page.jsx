adc750c7c246bec6e33d8cbe47deb33ac97d15fc
Viktor Kinyua <54580936+amature98@users.noreply.github.com>
2024-08-29 15:46:21 +0300

    More content changes

--- src/Pages/Jprc.page.jsx
+++ src/Pages/Jprc.page.jsx
@@ -8,7 +8,11 @@ function JprcPage() {
         groupName="JPRC"
         groupImage=""
         groupMotto="Fruitful Service with Humility and Servanthood (Matthew 7:18)"
-        aboutDescription={``}
+        aboutDescription={`God has called the church to champion Justice, Peace, Reconciliation, Human Dignity, and Environmental Stewardship. 
+          PCEA Mukinyi upholds the belief that God has entrusted humanity with the responsibility of being stewards of creation, drawing from it the resources needed for daily living.
+          JPRC’s establishment and mission are guided by key scriptures that emphasize the promotion and advocacy of justice, peace, reconciliation, and the recognition of humanity's role as co-workers in God's creation.
+          It was created to address issues that require the church's engagement with both State and Non-State Actors, providing critical input for the welfare of its members and the broader society.
+          The group's purpose is to educate members on the state of the nation and to encourage active involvement through training and public participation, empowering them to be informed citizens who understand their rights and responsibilities.`}
         aboutImage={null}
         ContactArray={[]}
         missionImage=""
