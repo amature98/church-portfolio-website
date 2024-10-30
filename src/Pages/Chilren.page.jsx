7182aeaaf0c75ea1db79a9b41d20af031b3cf5ad
Viktor Kinyua <54580936+amature98@users.noreply.github.com>
2024-10-30 19:14:09 +0300

    Beta changes

--- src/Pages/Chilren.page.jsx
+++ src/Pages/Chilren.page.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import GroupTemplate from "../Components/Template/Group.template";
 import { childrenCarousel } from "../Components/Arrays/Images.array";
 import GrpImg from '../Images/children.jpg'
+import AbtImg from '../Images/childrenAbt.jpg'
+import MsImg from '../Images/childrenMsImg.jpg'
 function ChilrenPage() {
   return (
     <>
@@ -11,10 +13,10 @@ function ChilrenPage() {
         groupMotto="LET THE LITTLE CHILDREN COME TO ME (LUKE 18:16)"
         aboutDescription={`This department is entrusted with the vital responsibility of imparting the Christian faith to the younger generation. It is structured around two fundamental pillars: Sunday School and Brigade, which are designed to nurture the spiritual growth of both boys and girls.
 Over the years, the department has made significant efforts to actively engage all relevant stakeholders, fostering a strong sense of unity and cooperation between its two primary pillars.`}
-        aboutImage={null}
+        aboutImage={AbtImg}
         ContactArray={[]}
         missionStatement="To provide holistic Christian instruction, training, and capacity building for all children and their mentors in order to accept Christ as their savior, equip them to understand their Christian faith and apply it in their lives."
-        missionImage={null}
+        missionImage={MsImg}
         visionStatement="To teach, and make visible to all children the Love of God and their holistic identity to Christ and their relationship to others."
         CarouselArray={childrenCarousel}
       />
--- src/Pages/HealthBoard.page.jsx
+++ src/Pages/HealthBoard.page.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import GroupTemplate from "../Components/Template/Group.template";
 import BgImg from "../Images/health.jpeg";
+import MsImg from '../Images/health1.jpg'
+import AbtImg from '../Images/health2.jpg'
 
 function HealthBoardPage() {
   return (
@@ -12,9 +14,9 @@ function HealthBoardPage() {
         aboutDescription={`The Health Board is a dedicated group within PCEA Mukinyi that focuses on promoting and maintaining the physical, mental, and spiritual well-being of both the members and the broader community. 
           Through education, support, and health initiatives, the Health Board seeks to address health-related concerns, encourage healthy lifestyles, and provide resources for individuals to achieve optimal health.
           The group also partners with healthcare professionals and organisations to offer screenings, workshops, and interventions that cater to the diverse health needs of the parish and its surrounding community, fostering a holistic approach to wellness that aligns with Christian principles.`}
-        aboutImage={null}
+        aboutImage={AbtImg}
         ContactArray={[]}
-        missionImage={null}
+        missionImage={MsImg}
         missionStatement="To enhance the well-being of our church and community by promoting healthy lifestyles, providing access to essential health resources, and fostering a supportive environment where physical, mental, and spiritual health are nurtured in alignment with Christian values."
         visionStatement="To create a thriving community where every individual experiences holistic health, empowered by knowledge, support, and faith-driven initiatives, ensuring that the church and its members are well-equipped to lead lives of wellness and service."
         CarouselArray={[]}
