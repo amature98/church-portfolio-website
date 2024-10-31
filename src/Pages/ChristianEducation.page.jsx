4a9dc7c22e2e9efdacacd5beea4bd10e87164fce
Viktor Kinyua <54580936+amature98@users.noreply.github.com>
2024-10-31 16:09:15 +0300

    More beta changes

--- src/Pages/ChristianEducation.page.jsx
+++ src/Pages/ChristianEducation.page.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import GroupTemplate from '../Components/Template/Group.template'
+import React from "react";
+import GroupTemplate from "../Components/Template/Group.template";
+import BgImg from "../Images/bible.jpg";
+import AbtImg from "../Images/cebg.jpg";
+import MsImg from "../Images/cems.jpg";
 
 function ChristianEducationPage() {
   return (
     <>
       <GroupTemplate
         groupName="Christian Education"
-        groupImage={null}
+        groupImage={BgImg}
         groupMotto={`MY PEOPLE ARE DESTROYED FROM LACK OF KNOWLEDGE’ (HOSEA 4:6)`}
         aboutDescription={`Christian Education is multifaceted, encompassing the roles of advising, facilitating, sensitizing, intervening, educating, and representing. The core objective of Christian Education within the Church is to teach, disciple, and equip learners to grow in Christlikeness. 
           At its essence, Christian Education is a Bible-based, Holy Spirit-empowered (Christ-centered) teaching and learning process. It aims to guide individuals through all stages of life, using contemporary teaching methods to help them discover and fulfill God's purpose through Christ in every aspect of living.
           We also functions as the Church's quality assurance arm, ensuring the application of Biblical principles across its institutions and departments. It collaborates with various groups and committees to uphold these standards.`}
-        aboutImage={null}
+        aboutImage={AbtImg}
         ContactArray={[]}
-        missionImage={null}
+        missionImage={MsImg}
         missionStatement="We endeavour to make disciples by instilling knowledge through Education for life."
         visionStatement="To have a society which has children and adults who are God-fearing and uphold the Christian values of love, moral integrity, self-control, and respect."
         CarouselArray={[]}
@@ -22,4 +25,4 @@ function ChristianEducationPage() {
   );
 }
 
-export default ChristianEducationPage
+export default ChristianEducationPage;
