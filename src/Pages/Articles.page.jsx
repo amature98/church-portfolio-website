a608c0080c0da557f8de174d2c2592d59f6b0745
kinyuaviktor <kinyuaviktor@gmail.com>
2025-04-12 09:17:28 +0000

    WP Integration

--- src/Pages/Articles.page.jsx
+++ src/Pages/Articles.page.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import {useState, useEffect}  from "react";
 import Page from "./Page";
 import Header from "../Components/Header/Header";
 import BgImage from "../Images/resources.jpg";
 
+import { Container, Card } from "@mui/material";
+
 function ArticlesPage() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pceamukinyi.com/wp-json/wp/v2/posts")
+      .then((res) => res.json())
+      .then((data) => setArticles(data))
+      .catch((err) => console.error("Error fetching posts", err));
+  }, []);
+
   return (
     <Page title="Articles - PCEA Mukinyi Parish">
       <Header
@@ -11,6 +22,16 @@ function ArticlesPage() {
         bgimage={BgImage}
         height="70vh"
       />
+      <Container>
+        {articles.map((article) => (
+          <Card key={article.id}>
+            <Card.Body>
+              <Card.Title>{article.title.rendered}</Card.Title>
+              <Card.Text>{article.excerpt.rendered}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </Container>
     </Page>
   );
 }
